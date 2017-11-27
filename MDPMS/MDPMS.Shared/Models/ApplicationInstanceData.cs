95386f6ae6eb02ff500245a1d42db4f0613cfc44
Nicholas Schoeb <nschoeb@impaqint.com>
2017-11-27 13:56:32 -0500

    added more models

---
+++ MDPMS/MDPMS.Shared/Models/ApplicationInstanceData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDPMS.Shared.Models
+{
+    public class ApplicationInstanceData
+    {
+        // PURPOSE: package instance data (cache, settings, etc.)     
+
+        public DateTime? LastSync { get; set; }
+        public string Url { get; set; }
+        public bool SyncError { get; set; }
+    }
+}
---
+++ MDPMS/MDPMS.Shared/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using MDPMS.Shared.Models;
+
+namespace MDPMS.Shared.ViewModels.Base
+{
+    public class ViewModelBase : ExtendedBindableObject
+    {
+        private bool _isBusy { get; set; }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {
+                if (value != _isBusy)
+                {
+                    _isBusy = value;
+                    RaisePropertyChanged(() => IsBusy);
+                }
+            }
+        }
+
+        private ApplicationInstanceData _applicationInstanceData;
+
+        public ApplicationInstanceData ApplicationInstanceData
+        {
+            get
+            {
+                return _applicationInstanceData;
+            }
+            set
+            {
+                _applicationInstanceData = value;
+                RaisePropertyChanged(() => ApplicationInstanceData);
+            }
+        }
+    }
+}
