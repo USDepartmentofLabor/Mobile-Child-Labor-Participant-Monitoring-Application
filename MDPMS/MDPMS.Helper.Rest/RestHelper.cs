55a69e975b30f943356804d2dd353c9501d52b32
Nicholas Schoeb <nschoeb@impaqint.com>
2017-12-21 15:23:13 -0500

    added initial MDPMS.Helper.Rest proj using RestSharp

---
+++ MDPMS/MDPMS.Helper.Rest/RestHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace MDPMS.Helper.Rest
+{
+    public static class RestHelper
+    {
+        public static string PerformRestGetRequestWithHttpBasicAuth(string url, string apiPath, string username, string password)
+        {
+            var client = new RestClient(url) { Authenticator = new HttpBasicAuthenticator(username, password) };
+            var request = new RestRequest(apiPath, Method.GET);
+            var response = client.Execute(request);            
+            if (response.IsSuccessful) { return response.Content; }
+            throw new Exception(@"Unsuccessful REST Request");            
+        }
+    }
+}
