de304675f125f4cced983ca94b43d7184762ce8e
Nicholas Schoeb <nschoeb@impaqint.com>
2017-12-29 11:50:04 -0500

    added households name view and search

--- MDPMS/MDPMS.Shared/ViewModels/HouseholdsViewModel.cs
+++ MDPMS/MDPMS.Shared/ViewModels/HouseholdsViewModel.cs
@@ -1,13 +1,63 @@
-﻿using MDPMS.Shared.Models;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using MDPMS.Database.Data.Models;
+using MDPMS.Shared.Models;
 using MDPMS.Shared.ViewModels.Base;
+using Xamarin.Forms;
 
 namespace MDPMS.Shared.ViewModels
 {
     public class HouseholdsViewModel : ViewModelBase
     {
+        public string SearchText { get; set; } = @"";
+        public ObservableCollection<Household> Households { get; set; }
+        public Household SelectedHousehold { get; set; } = null;
+
+        private bool _isRefreshing;
+        public bool IsRefreshing
+        {
+            get => _isRefreshing;
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+
+        public Command RefreshCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    IsRefreshing = true;
+                    await Task.Run(() => { LoadHouseholds(); });
+                    IsRefreshing = false;
+                });
+            }
+        }
+
+        public Command SearchCommand { get; set; }
+
         public HouseholdsViewModel(ApplicationInstanceData applicationInstanceData)
         {
             ApplicationInstanceData = applicationInstanceData;
+            SearchCommand = new Command(LoadHouseholds);
+            LoadHouseholds();
         }
+        
+        private void LoadHouseholds()
+        {
+            Households = new ObservableCollection<Household>();
+            var query = SearchText.Equals(string.Empty) ? ApplicationInstanceData.Data.Households : ApplicationInstanceData.Data.Households.Where(a => a.HouseholdName.Contains(SearchText));
+            foreach (var household in query)
+            {
+                Households.Add(household);
+            }
+            if (Households.Any()) SelectedHousehold = Households.First();
+            OnPropertyChanged(nameof(Households));
+            OnPropertyChanged(nameof(SelectedHousehold));
+        } 
     }
 }
