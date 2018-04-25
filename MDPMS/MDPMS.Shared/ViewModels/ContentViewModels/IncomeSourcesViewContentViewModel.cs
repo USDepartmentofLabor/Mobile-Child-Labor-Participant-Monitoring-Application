d0556e7a8333a5561a3d14be3407eb73432f3119
Nicholas Schoeb <nschoeb@impaqint.com>
2018-04-25 14:38:53 -0400

    show hide add income source button on income sources view content view if parent household not yet synced

--- MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/IncomeSourcesViewContentViewModel.cs
+++ MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/IncomeSourcesViewContentViewModel.cs
@@ -3,11 +3,16 @@ using MDPMS.Shared.Models;
 using MDPMS.Shared.ViewModels.Base;
 using MDPMS.Shared.ViewModels.ContentPageModels;
 using MDPMS.Shared.Views.ContentPages;
+using Xamarin.Forms;
 
 namespace MDPMS.Shared.ViewModels.ContentViewModels
 {
     public class IncomeSourcesViewContentViewModel : ViewModelBase
     {
+        public Command AddIncomeSourceCommand { get; set; }
+
+        public GridLength AllowAddIncomeSourceButtonRowHeight { get; set; }
+
         public Household Household { get; set; }
 
         private IncomeSource _selectedIncomeSource;
@@ -29,6 +34,18 @@ namespace MDPMS.Shared.ViewModels.ContentViewModels
         {
             ApplicationInstanceData = applicationInstanceData;
             Household = household;
+
+            AllowAddIncomeSourceButtonRowHeight = Household.HasExternalId ? 0 : 80;
+
+            AddIncomeSourceCommand = new Command(ExecuteAddIncomeSourceCommand);
+        }
+
+        private void ExecuteAddIncomeSourceCommand()
+        {
+            // is it allowed?
+            if (Household.HasExternalId) return;
+
+            // go to add view here
         }
     }
 }
