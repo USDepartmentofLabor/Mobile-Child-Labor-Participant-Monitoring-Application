fc45bb9c223ba347b17d33fc0c0b991353fd8b0f
Nicholas Schoeb <nschoeb@impaqint.com>
2018-05-03 17:12:32 -0400

    show all fields on person view

--- MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/PersonViewContentViewModel.cs
+++ MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/PersonViewContentViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MDPMS.Database.Data.Models;
 using MDPMS.Shared.Models;
 using MDPMS.Shared.ViewModels.Base;
+using MDPMS.Shared.ViewModels.Helpers;
 
 namespace MDPMS.Shared.ViewModels.ContentViewModels
 {
@@ -12,10 +13,24 @@ namespace MDPMS.Shared.ViewModels.ContentViewModels
 
         public string IsTheBirthdayAnApproximateDateTranslated { get { return ApplicationInstanceData.SelectedLocalization.Translations[Person.DateOfBirthIsApproximate.ToString()]; } }
 
+        public string WorkActivitiesReadableList { get; set; }
+        public string HazardousConditionsReadableList { get; set; }
+        public string HouseholdTasksReadableList { get; set; }
+
+        public string HaveJobReturningToReadable { get; set; }
+        public string EnrolledInSchoolReadable { get; set; }
+
         public PersonViewContentViewModel(ApplicationInstanceData applicationInstanceData, Person person)
         {
             ApplicationInstanceData = applicationInstanceData;
             Person = person;
+
+            WorkActivitiesReadableList = StatusCustomizationValueConverter.GetWorkActivitiesReadableList(Person, ApplicationInstanceData);
+            HazardousConditionsReadableList = StatusCustomizationValueConverter.GetHazardousConditionsReadableList(Person, ApplicationInstanceData);
+            HouseholdTasksReadableList = StatusCustomizationValueConverter.GetHouseholdTasksReadableList(person, ApplicationInstanceData);
+
+            HaveJobReturningToReadable = ViewModelDisplayValueConverter.GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, Person.HaveJobReturningTo);
+            EnrolledInSchoolReadable = ViewModelDisplayValueConverter.GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, Person.EnrolledInSchool);
         }
     }
 }
--- MDPMS/MDPMS.Shared/ViewModels/Helpers/StatusCustomizationValueConverter.cs
+++ MDPMS/MDPMS.Shared/ViewModels/Helpers/StatusCustomizationValueConverter.cs
@@ -7,6 +7,20 @@ namespace MDPMS.Shared.ViewModels.Helpers
 {
     public static class StatusCustomizationValueConverter
     {
+        public static string GetWorkActivitiesReadableList(Person person, ApplicationInstanceData applicationInstanceData)
+        {
+            var sb = new StringBuilder();
+            var i = 0;
+            var workActivityCount = person.PeopleWorkActivities.Count();
+            foreach (var workActivity in person.PeopleWorkActivities)
+            {
+                sb.Append(applicationInstanceData.Data.StatusCustomizationWorkActivities.First(a => a.InternalId == workActivity.WorkActivityInternalId).DisplayName);
+                if (i != (workActivityCount - 1)) sb.Append(@", ");
+                i++;
+            }
+            return sb.ToString();
+        }
+
         public static string GetWorkActivitiesReadableList(PersonFollowUp personFollowUp, ApplicationInstanceData applicationInstanceData)
         {
             var sb = new StringBuilder();
@@ -21,6 +35,20 @@ namespace MDPMS.Shared.ViewModels.Helpers
             return sb.ToString();
         }
 
+        public static string GetHazardousConditionsReadableList(Person person, ApplicationInstanceData applicationInstanceData)
+        {
+            var sb = new StringBuilder();
+            var i = 0;
+            var objectCount = person.PeopleHazardousConditions.Count();
+            foreach (var workActivity in person.PeopleHazardousConditions)
+            {
+                sb.Append(applicationInstanceData.Data.StatusCustomizationHazardousConditions.First(a => a.InternalId == workActivity.HazardousConditionInternalId).DisplayName);
+                if (i != (objectCount - 1)) sb.Append(@", ");
+                i++;
+            }
+            return sb.ToString();
+        }
+
         public static string GetHazardousConditionsReadableList(PersonFollowUp personFollowUp, ApplicationInstanceData applicationInstanceData)
         {
             var sb = new StringBuilder();
@@ -29,6 +57,20 @@ namespace MDPMS.Shared.ViewModels.Helpers
             foreach (var workActivity in personFollowUp.PeopleFollowUpHazardousConditions)
             {
                 sb.Append(applicationInstanceData.Data.StatusCustomizationHazardousConditions.First(a => a.InternalId == workActivity.HazardousConditionInternalId).DisplayName);
+                if (i != (objectCount - 1)) sb.Append(@", ");
+                i++;
+            }
+            return sb.ToString();
+        }
+
+        public static string GetHouseholdTasksReadableList(Person person, ApplicationInstanceData applicationInstanceData)
+        {
+            var sb = new StringBuilder();
+            var i = 0;
+            var objectCount = person.PeopleHouseholdTasks.Count();
+            foreach (var workActivity in person.PeopleHouseholdTasks)
+            {
+                sb.Append(applicationInstanceData.Data.StatusCustomizationHouseholdTasks.First(a => a.InternalId == workActivity.HouseholdTaskInternalId).DisplayName);
                 if (i != (objectCount - 1)) sb.Append(@", ");
                 i++;
             }
