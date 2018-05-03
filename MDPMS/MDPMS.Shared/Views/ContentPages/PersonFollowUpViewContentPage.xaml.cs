5dcf65f9afc73a6310673858b8b4b0e4089b37ff
Nicholas Schoeb <nschoeb@impaqint.com>
2018-05-03 18:06:33 -0400

    show custom values on person follow up view

--- MDPMS/MDPMS.Shared/Views/ContentPages/PersonFollowUpViewContentPage.xaml.cs
+++ MDPMS/MDPMS.Shared/Views/ContentPages/PersonFollowUpViewContentPage.xaml.cs
@@ -21,7 +21,7 @@ namespace MDPMS.Shared.Views.ContentPages
             var childViewModel = new PersonFollowUpViewContentViewModel(viewModel.ApplicationInstanceData, viewModel.PersonFollowUp);
             childView.BindingContext = childViewModel;
             this.ScrollView.Content = childView;
-            //childView.OnAppearing();
+            childView.OnAppearing();
         }
     }
 }
--- MDPMS/MDPMS.Shared/Views/ContentViews/PersonFollowUpViewContentView.xaml.cs
+++ MDPMS/MDPMS.Shared/Views/ContentViews/PersonFollowUpViewContentView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System.Linq;
+using MDPMS.Shared.ViewModels.ContentViewModels;
+using MDPMS.Shared.ViewModels.Helpers;
+using Xamarin.Forms;
 
 namespace MDPMS.Shared.Views.ContentViews
 {
@@ -7,6 +10,91 @@ namespace MDPMS.Shared.Views.ContentViews
         public PersonFollowUpViewContentView()
         {
             InitializeComponent();
+        }
+
+        public void OnAppearing()
+        {
+            var viewModel = (PersonFollowUpViewContentViewModel)BindingContext;
+
+            // Custom Fields
+            CustomFieldContent.Children.Clear();
+
+            var grid = new Grid();
+            var labelColumnDefinition = new ColumnDefinition();
+            labelColumnDefinition.Width = 200;
+            grid.ColumnDefinitions.Add(labelColumnDefinition);
+            var valueColumnDefinition = new ColumnDefinition();
+            grid.ColumnDefinitions.Add(valueColumnDefinition);
+
+            var customFields = viewModel.ApplicationInstanceData.Data.CustomFields
+                                            .Where(a => a.ModelType.Equals(@"FollowUp"))
+                                            .OrderBy(b => b.SortOrder);
+            var i = 0;
+            foreach (var customField in customFields)
+            {
+                var rowDefinition = new RowDefinition();
+                rowDefinition.Height = 30;
+                var valueQuery = viewModel.ApplicationInstanceData.Data.CustomPersonFollowUpValues
+                                          .Where(a => (a.CustomField.InternalId == customField.InternalId))
+                                          .Where(b => b.PersonFollowUp.InternalId == viewModel.PersonFollowUp.InternalId);
+                var queryCount = valueQuery.Any() ? valueQuery.Count() : 0;
+                var valueString = @"";
+                if (queryCount > 1) { /* TODO: error log */ }
+                if (queryCount.Equals(1))
+                {
+                    var value = valueQuery.First();
+                    valueString = value.Value;
+                    switch (customField.FieldType)
+                    {
+                        case @"text":
+                            valueString = CustomValueConverter.GetValueFromJsonText(valueString);
+                            break;
+                        case @"textarea":
+                            valueString = CustomValueConverter.GetValueFromJsonTextArea(valueString);
+                            rowDefinition.Height = GridLength.Auto;
+                            break;
+                        case @"check_box":
+                            valueString = CustomValueConverter.GetValueFromJsonCheckBox(valueString);
+                            rowDefinition.Height = GridLength.Auto;
+                            break;
+                        case @"radio_button":
+                            valueString = CustomValueConverter.GetValueFromJsonRadioButton(valueString);
+                            break;
+                        case @"select":
+                            valueString = CustomValueConverter.GetValueFromJsonSelect(valueString);
+                            break;
+                        case @"number":
+                            valueString = CustomValueConverter.GetValueFromJsonNumber(valueString).ToString();
+                            break;
+                        case @"date":
+                            valueString = CustomValueConverter.GetValueFromJsonDate(valueString).ToShortDateString();
+                            break;
+                        case @"rank_list":
+                            valueString = CustomValueConverter.GetDisplayValueFromJsonRankList(valueString);
+                            rowDefinition.Height = GridLength.Auto;
+                            break;
+                    }
+                }
+
+                grid.RowDefinitions.Add(rowDefinition);
+
+                var labelLabel = new Label();
+                labelLabel.Text = customField.Name;
+                labelLabel.SetValue(Grid.ColumnProperty, 0);
+                labelLabel.SetValue(Grid.RowProperty, i);
+                labelLabel.Style = (Style)Resources["LabelDescription"];
+                grid.Children.Add(labelLabel);
+
+                var valueLabel = new Label();
+                valueLabel.Text = valueString;
+                valueLabel.SetValue(Grid.ColumnProperty, 1);
+                valueLabel.SetValue(Grid.RowProperty, i);
+                valueLabel.Style = (Style)Resources["LabelValue"];
+                grid.Children.Add(valueLabel);
+
+                i++;
+            }
+            CustomFieldContent.Children.Add(grid);
         }
     }
 }
