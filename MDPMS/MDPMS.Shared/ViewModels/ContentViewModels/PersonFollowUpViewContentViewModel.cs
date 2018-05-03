9d5e91276d8e6bf9dcef7652d99e32676c0a056e
Nicholas Schoeb <nschoeb@impaqint.com>
2018-05-03 16:23:33 -0400

    move value conversion to helper

--- MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/PersonFollowUpViewContentViewModel.cs
+++ MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/PersonFollowUpViewContentViewModel.cs
@@ -25,14 +25,8 @@ namespace MDPMS.Shared.ViewModels.ContentViewModels
             HazardousConditionsReadableList = StatusCustomizationValueConverter.GetHazardousConditionsReadableList(PersonFollowUp, ApplicationInstanceData);
             HouseholdTasksReadableList = StatusCustomizationValueConverter.GetHouseholdTasksReadableList(personFollowUp, ApplicationInstanceData);
 
-            HaveJobReturningToReadable = GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, PersonFollowUp.HaveJobReturningTo);
-            EnrolledInSchoolReadable = GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, PersonFollowUp.EnrolledInSchool);
-        }
-
-        private string GetBooleanTranslated(Localization localization, bool? value)
-        {
-            if (value == null) return @"";
-            return localization.Translations[(bool)value ? @"Yes" : @"No"];
+            HaveJobReturningToReadable = ViewModelDisplayValueConverter.GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, PersonFollowUp.HaveJobReturningTo);
+            EnrolledInSchoolReadable = ViewModelDisplayValueConverter.GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, PersonFollowUp.EnrolledInSchool);
         }
     }
 }
---
+++ MDPMS/MDPMS.Shared/ViewModels/Helpers/ViewModelDisplayValueConverter.cs
@@ -0,0 +1,13 @@
+﻿using MDPMS.Shared.Models;
+
+namespace MDPMS.Shared.ViewModels.Helpers
+{
+    public static class ViewModelDisplayValueConverter
+    {
+        public static string GetBooleanTranslated(Localization localization, bool? value)
+        {
+            if (value == null) return @"";
+            return localization.Translations[(bool)value ? @"Yes" : @"No"];
+        }
+    }
+}
