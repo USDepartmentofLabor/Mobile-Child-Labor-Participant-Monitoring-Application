1aa5f660d0a916887d58ca0b901fc759fff4192b
Nicholas Schoeb <nschoeb@impaqint.com>
2018-05-03 16:18:42 -0400

    move status customization display value converter functions to helper

--- MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/PersonFollowUpViewContentViewModel.cs
+++ MDPMS/MDPMS.Shared/ViewModels/ContentViewModels/PersonFollowUpViewContentViewModel.cs
@@ -1,8 +1,7 @@
-﻿using System.Linq;
-using System.Text;
-using MDPMS.Database.Data.Models;
+﻿using MDPMS.Database.Data.Models;
 using MDPMS.Shared.Models;
 using MDPMS.Shared.ViewModels.Base;
+using MDPMS.Shared.ViewModels.Helpers;
 
 namespace MDPMS.Shared.ViewModels.ContentViewModels
 {
@@ -22,54 +21,12 @@ namespace MDPMS.Shared.ViewModels.ContentViewModels
             ApplicationInstanceData = applicationInstanceData;
             PersonFollowUp = personFollowUp;
 
-            WorkActivitiesReadableList = GetWorkActivitiesReadableList(PersonFollowUp);
-            HazardousConditionsReadableList = GetHazardousConditionsReadableList(PersonFollowUp);
-            HouseholdTasksReadableList = GetHouseholdTasksReadableList(personFollowUp);
+            WorkActivitiesReadableList = StatusCustomizationValueConverter.GetWorkActivitiesReadableList(PersonFollowUp, ApplicationInstanceData);
+            HazardousConditionsReadableList = StatusCustomizationValueConverter.GetHazardousConditionsReadableList(PersonFollowUp, ApplicationInstanceData);
+            HouseholdTasksReadableList = StatusCustomizationValueConverter.GetHouseholdTasksReadableList(personFollowUp, ApplicationInstanceData);
 
             HaveJobReturningToReadable = GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, PersonFollowUp.HaveJobReturningTo);
             EnrolledInSchoolReadable = GetBooleanTranslated(ApplicationInstanceData.SelectedLocalization, PersonFollowUp.EnrolledInSchool);
-        }
-
-        private string GetWorkActivitiesReadableList(PersonFollowUp personFollowUp)
-        {
-            var sb = new StringBuilder();
-            var i = 0;
-            var workActivityCount = personFollowUp.PeopleFollowUpWorkActivities.Count();
-            foreach (var workActivity in personFollowUp.PeopleFollowUpWorkActivities)
-            {
-                sb.Append(ApplicationInstanceData.Data.StatusCustomizationWorkActivities.First(a => a.InternalId == workActivity.WorkActivityInternalId).DisplayName);
-                if (i != (workActivityCount - 1)) sb.Append(@", ");
-                i++;
-            }
-            return sb.ToString();
-        }
-
-        private string GetHazardousConditionsReadableList(PersonFollowUp personFollowUp)
-        {
-            var sb = new StringBuilder();
-            var i = 0;
-            var objectCount = personFollowUp.PeopleFollowUpHazardousConditions.Count();
-            foreach (var workActivity in personFollowUp.PeopleFollowUpHazardousConditions)
-            {
-                sb.Append(ApplicationInstanceData.Data.StatusCustomizationHazardousConditions.First(a => a.InternalId == workActivity.HazardousConditionInternalId).DisplayName);
-                if (i != (objectCount - 1)) sb.Append(@", ");
-                i++;
-            }
-            return sb.ToString();
-        }
-
-        private string GetHouseholdTasksReadableList(PersonFollowUp personFollowUp)
-        {
-            var sb = new StringBuilder();
-            var i = 0;
-            var objectCount = personFollowUp.PeopleFollowUpHouseholdTasks.Count();
-            foreach (var workActivity in personFollowUp.PeopleFollowUpHouseholdTasks)
-            {
-                sb.Append(ApplicationInstanceData.Data.StatusCustomizationHouseholdTasks.First(a => a.InternalId == workActivity.HouseholdTaskInternalId).DisplayName);
-                if (i != (objectCount - 1)) sb.Append(@", ");
-                i++;
-            }
-            return sb.ToString();
         }
 
         private string GetBooleanTranslated(Localization localization, bool? value)
---
+++ MDPMS/MDPMS.Shared/ViewModels/Helpers/StatusCustomizationValueConverter.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Text;
+using MDPMS.Database.Data.Models;
+using MDPMS.Shared.Models;
+
+namespace MDPMS.Shared.ViewModels.Helpers
+{
+    public static class StatusCustomizationValueConverter
+    {
+        public static string GetWorkActivitiesReadableList(PersonFollowUp personFollowUp, ApplicationInstanceData applicationInstanceData)
+        {
+            var sb = new StringBuilder();
+            var i = 0;
+            var workActivityCount = personFollowUp.PeopleFollowUpWorkActivities.Count();
+            foreach (var workActivity in personFollowUp.PeopleFollowUpWorkActivities)
+            {
+                sb.Append(applicationInstanceData.Data.StatusCustomizationWorkActivities.First(a => a.InternalId == workActivity.WorkActivityInternalId).DisplayName);
+                if (i != (workActivityCount - 1)) sb.Append(@", ");
+                i++;
+            }
+            return sb.ToString();
+        }
+
+        public static string GetHazardousConditionsReadableList(PersonFollowUp personFollowUp, ApplicationInstanceData applicationInstanceData)
+        {
+            var sb = new StringBuilder();
+            var i = 0;
+            var objectCount = personFollowUp.PeopleFollowUpHazardousConditions.Count();
+            foreach (var workActivity in personFollowUp.PeopleFollowUpHazardousConditions)
+            {
+                sb.Append(applicationInstanceData.Data.StatusCustomizationHazardousConditions.First(a => a.InternalId == workActivity.HazardousConditionInternalId).DisplayName);
+                if (i != (objectCount - 1)) sb.Append(@", ");
+                i++;
+            }
+            return sb.ToString();
+        }
+
+        public static string GetHouseholdTasksReadableList(PersonFollowUp personFollowUp, ApplicationInstanceData applicationInstanceData)
+        {
+            var sb = new StringBuilder();
+            var i = 0;
+            var objectCount = personFollowUp.PeopleFollowUpHouseholdTasks.Count();
+            foreach (var workActivity in personFollowUp.PeopleFollowUpHouseholdTasks)
+            {
+                sb.Append(applicationInstanceData.Data.StatusCustomizationHouseholdTasks.First(a => a.InternalId == workActivity.HouseholdTaskInternalId).DisplayName);
+                if (i != (objectCount - 1)) sb.Append(@", ");
+                i++;
+            }
+            return sb.ToString();
+        }
+    }
+}
