620fdb393ea64ba541adc3707c610310740e5b6c
Nicholas Schoeb <nschoeb@impaqint.com>
2018-05-03 17:35:50 -0400

    display custom values on person view

--- MDPMS/MDPMS.Shared/Views/ContentViews/PersonViewContentView.xaml.cs
+++ MDPMS/MDPMS.Shared/Views/ContentViews/PersonViewContentView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MDPMS.Shared.ViewModels.ContentViewModels;
+﻿using System.Linq;
+using MDPMS.Shared.ViewModels.ContentViewModels;
+using MDPMS.Shared.ViewModels.Helpers;
 using Xamarin.Forms;
 
 namespace MDPMS.Shared.Views.ContentViews
@@ -13,6 +15,86 @@ namespace MDPMS.Shared.Views.ContentViews
         public void OnAppearing()
         {
             var viewModel = (PersonViewContentViewModel)BindingContext;
+
+            // Custom Fields
+            CustomFieldContent.Children.Clear();
+
+            var grid = new Grid();
+            var labelColumnDefinition = new ColumnDefinition();
+            labelColumnDefinition.Width = 200;
+            grid.ColumnDefinitions.Add(labelColumnDefinition);
+            var valueColumnDefinition = new ColumnDefinition();
+            grid.ColumnDefinitions.Add(valueColumnDefinition);
+
+            var customFields = viewModel.ApplicationInstanceData.Data.CustomFields
+                                            .Where(a => a.ModelType.Equals(@"Person"))
+                                            .OrderBy(b => b.SortOrder);
+            var i = 0;
+            foreach (var customField in customFields)
+            {
+                var rowDefinition = new RowDefinition();
+                rowDefinition.Height = 30;
+                var valueQuery = viewModel.ApplicationInstanceData.Data.CustomPersonValues
+                                          .Where(a => (a.CustomField.InternalId == customField.InternalId))
+                                          .Where(b => b.Person.InternalId == viewModel.Person.InternalId);
+                var queryCount = valueQuery.Any() ? valueQuery.Count() : 0;
+                var valueString = @"";
+                if (queryCount > 1) { /* TODO: error log */ }
+                if (queryCount.Equals(1))
+                {
+                    var value = valueQuery.First();
+                    valueString = value.Value;
+                    switch (customField.FieldType)
+                    {
+                        case @"text":
+                            valueString = CustomValueConverter.GetValueFromJsonText(valueString);
+                            break;
+                        case @"textarea":
+                            valueString = CustomValueConverter.GetValueFromJsonTextArea(valueString);
+                            rowDefinition.Height = GridLength.Auto;
+                            break;
+                        case @"check_box":
+                            valueString = CustomValueConverter.GetValueFromJsonCheckBox(valueString);
+                            rowDefinition.Height = GridLength.Auto;
+                            break;
+                        case @"radio_button":
+                            valueString = CustomValueConverter.GetValueFromJsonRadioButton(valueString);
+                            break;
+                        case @"select":
+                            valueString = CustomValueConverter.GetValueFromJsonSelect(valueString);
+                            break;
+                        case @"number":
+                            valueString = CustomValueConverter.GetValueFromJsonNumber(valueString).ToString();
+                            break;
+                        case @"date":
+                            valueString = CustomValueConverter.GetValueFromJsonDate(valueString).ToShortDateString();
+                            break;
+                        case @"rank_list":
+                            valueString = CustomValueConverter.GetDisplayValueFromJsonRankList(valueString);
+                            rowDefinition.Height = GridLength.Auto;
+                            break;
+                    }
+                }
+
+                grid.RowDefinitions.Add(rowDefinition);
+
+                var labelLabel = new Label();
+                labelLabel.Text = customField.Name;
+                labelLabel.SetValue(Grid.ColumnProperty, 0);
+                labelLabel.SetValue(Grid.RowProperty, i);
+                labelLabel.Style = (Style)Resources["LabelDescription"];
+                grid.Children.Add(labelLabel);
+
+                var valueLabel = new Label();
+                valueLabel.Text = valueString;
+                valueLabel.SetValue(Grid.ColumnProperty, 1);
+                valueLabel.SetValue(Grid.RowProperty, i);
+                valueLabel.Style = (Style)Resources["LabelValue"];
+                grid.Children.Add(valueLabel);
+
+                i++;
+            }
+            CustomFieldContent.Children.Add(grid);
 
             // Person Follow Ups
             PersonFollowUpsContent.Children.Clear();
