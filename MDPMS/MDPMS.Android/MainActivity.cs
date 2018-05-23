b09e61ba6e6d353758789680e907e46e6d3a17c2
Nicholas Schoeb <nschoeb@impaqint.com>
2018-05-23 14:09:31 -0400

    change launch option to single instance, fix behavior where launching from app menu while alrady launched would init app

--- MDPMS/MDPMS.Android/MainActivity.cs
+++ MDPMS/MDPMS.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MDPMS.Droid
 {
-	[Activity (Label = "MDPMS", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "MDPMS", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleInstance)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
